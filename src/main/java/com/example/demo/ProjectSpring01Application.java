12f56c0ecca975874755628d2949c85ff228d377
LauraGarciaBonet <laura-dinorah.garcia-bonet@capgemini.com>
2021-11-29 21:50:14 +0100

    project spring

---
+++ src/main/java/com/example/demo/ProjectSpring01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpring01Application.class, args);
+	}
+
+}
